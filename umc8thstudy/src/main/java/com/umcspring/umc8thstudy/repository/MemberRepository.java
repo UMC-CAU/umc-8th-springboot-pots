ebe2b6f40d6342007adbd516e969b85e8acd1c37
임동재 <djlim1998@gmail.com>
2025-05-07 21:16:50 +0900

    feature: MemberRepository 추가

--- umc8thstudy/src/main/java/com/umcspring/umc8thstudy/repository/MemberRepository.java
+++ umc8thstudy/src/main/java/com/umcspring/umc8thstudy/repository/MemberRepository.java
@@ -1,2 +1,9 @@
-package com.umcspring.umc8thstudy.repository;public interface MemberRepository {
+package com.umcspring.umc8thstudy.repository;
+
+import com.umcspring.umc8thstudy.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
