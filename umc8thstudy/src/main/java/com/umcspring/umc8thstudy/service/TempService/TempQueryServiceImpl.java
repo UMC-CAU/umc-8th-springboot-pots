950fa2cdadffd27d8986a69115dcc6716eae6ab4
임동재 <djlim1998@gmail.com>
2025-05-14 18:04:24 +0900

    feat: 에러핸들러 테스트 api 추가

---
+++ umc8thstudy/src/main/java/com/umcspring/umc8thstudy/service/TempService/TempQueryServiceImpl.java
@@ -0,0 +1,17 @@
+package com.umcspring.umc8thstudy.service.TempService;
+
+import com.umcspring.umc8thstudy.apiPayload.code.status.ErrorStatus;
+import com.umcspring.umc8thstudy.apiPayload.exception.handler.TempHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempQueryServiceImpl implements TempQueryService{
+
+    @Override
+    public void CheckFlag(Integer flag) {
+        if (flag == 1)
+            throw new TempHandler(ErrorStatus.TEMP_EXCEPTION);
+    }
+}
--- umc8thstudy/src/main/java/com/umcspring/umc8thstudy/web/controller/TempRestController.java
+++ umc8thstudy/src/main/java/com/umcspring/umc8thstudy/web/controller/TempRestController.java
@@ -2,10 +2,12 @@ package com.umcspring.umc8thstudy.web.controller;
 
 import com.umcspring.umc8thstudy.apiPayload.ApiResponse;
 import com.umcspring.umc8thstudy.converter.TempConverter;
+import com.umcspring.umc8thstudy.service.TempService.TempQueryService;
 import com.umcspring.umc8thstudy.web.dto.TempResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TempRestController {
 
+    private final TempQueryService tempQueryService;
+
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI(){
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam(name = "flag") Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }
